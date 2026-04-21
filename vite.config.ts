2f0b7f875c9e55ba2b54f61760c4199bb9436777
bea94k <59803918+bea94k@users.noreply.github.com>
2026-04-21 17:41:31 +0300

    Set up CI and deployment with GH Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // on GitHub Pages, the app is served from "/<repo-name>/" 
+  base: process.env.GITHUB_ACTIONS ? "/weather-forecast/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
